f0393013b9f8448cdda15c4cc74efbba49517a47
Gabri <gabrim90@gmail.com>
2023-01-21 08:45:17 +0100

    quito trazas

--- todo-all-todo/components/createdLists.jsx
+++ todo-all-todo/components/createdLists.jsx
@@ -3,7 +3,6 @@ import createClient from '../utils/supabase-server'
 export default async function CreatedLists(props){
     const supabase = createClient()
     const { data, error } = await supabase.from('lists').select()
-    console.log(data)
 
     const createdLists = data.map((list) => {
         return (
